8d3d4d26b2508a7d52e863e29b146f7b50b15094
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2024-03-17 22:41:26 -0600

    Fix the broken cards, and Add an animation to improve the design

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,19 +1,23 @@
-// import Swiper core and required modules
 import { EffectCards } from "swiper/modules";
 
 import Git from "../../public/Events/Git.jpeg";
 import WelcomeBack from "../../public/Events/WelcomeBack.jpeg";
 
-type Image = {
+type Card = {
   src: string;
   name: string;
   description?: string;
 };
 
-const images: Image[] = [
+const card: Card[] = [
   {
     src: Git.src,
-    name: "Git desde Cero",
+    name: "IEEE - ESTl Student Workshops",
+    description: "Taller: Git desde Cero",
+  },
+  {
+    src: WelcomeBack.src,
+    name: "Welcome Back",
     description: "",
   },
   {
@@ -23,25 +27,44 @@ const images: Image[] = [
   },
 ];
 
-import "./Events.css";
+import "./cssEvents/Events.css";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
+const buildStyle = ({ animation }: { animation?: string }) => {
+  let style = { "--animation": animation } as React.CSSProperties;
+
+  return style;
+};
+
 export default function App() {
   return (
     <>
       <div className="container" id="Eventos">
-        <div className="events">
-          <h2 className="gray">Conferecias</h2>
-          <h2>Tallereres</h2>
-          <h2 className="gray">Competencias</h2>
-        </div>
+        <h2 className="events">
+          <span className="span-events" data-content="Conferecias">
+            Conferecias
+          </span>
+          <span
+            className="span-events"
+            data-content="Talleres"
+            style={buildStyle({ animation: "a2" })}
+          >
+            Talleres
+          </span>
+          <span
+            className="span-events"
+            data-content="Hackathon"
+            style={buildStyle({ animation: "a3" })}
+          >
+            Hackathon
+          </span>
+        </h2>
 
         <Swiper
           effect={"cards"}
@@ -49,11 +72,11 @@ export default function App() {
           modules={[EffectCards]}
           className="mySwiper"
         >
-          {images.map((image, index) => (
+          {card.map((Card, index) => (
             <SwiperSlide key={index}>
-              <h1>{image.name}</h1>
-              <p>{image.description}</p>
-              <img src={image.src} alt={image.name} />
+              <h1 className="eventName">{Card.name}</h1>
+              <p className="eventDescription">{Card.description}</p>
+              <img src={Card.src} alt={Card.name} />
             </SwiperSlide>
           ))}
         </Swiper>
